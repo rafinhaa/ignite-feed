c7b34e6b58b68c3d8380d0d98c49e3d481c06211
rafinhaa <rafinhaa@me.com>
2022-07-05 08:58:47 -0300

    chore: Componente e propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Post } from "./Post";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <Post
+        author="John"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui cumque modi sunt accusamus dolore hic rem culpa. Maiores, illum voluptates! Quisquam officiis illo commodi deleniti quasi maiores ab rem in."
+      />
+      <Post
+        author="Jane"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, tenetur dicta quis totam nemo nostrum, repellendus et qui fugit quibusdam expedita quisquam a? Ipsum sunt consequatur repellat, libero magnam impedit!"
+      />
+    </div>
+  );
 }
 
 export default App;
