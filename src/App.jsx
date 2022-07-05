ac4e45b6dc1300dcd24c28a1d2fce42c05fd7689
rafinhaa <rafinhaa@me.com>
2022-07-05 09:12:36 -0300

    ReactJS - Ignite Feed - CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./styles.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Post
         author="John"
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui cumque modi sunt accusamus dolore hic rem culpa. Maiores, illum voluptates! Quisquam officiis illo commodi deleniti quasi maiores ab rem in."
