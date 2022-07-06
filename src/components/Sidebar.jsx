98e40d39c35eb7680ef022203f334ce6739dbffc
rafinhaa <rafinhaa@me.com>
2022-07-06 10:50:55 -0300

    ReactJS - Ignite Feed - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react";
+
 import styles from "./Sidebar.module.css";
 
 export const Sidebar = () => {
@@ -6,14 +8,22 @@ export const Sidebar = () => {
       <img
         className={styles.cover}
         src="https://images.unsplash.com/photo-1506878206813-92402b8ded23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZGV2ZWxvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=50"
-        alt="avatar"
+        alt="cover"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/rafinhaa.png"
+          alt="avatar"
+        />
         <strong>Rafinhaa</strong>
         <span>Software Engineer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
