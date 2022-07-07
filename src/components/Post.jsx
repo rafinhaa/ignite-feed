f54eadce64babbec62c0ecc3f8e95fac88977355
rafinhaa <rafinhaa@me.com>
2022-07-07 10:31:37 -0300

    ReactJS - Ignite Feed - Estilização do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,40 +2,37 @@ import styles from "./Post.module.css";
 
 export const Post = ({ author, content }) => {
   return (
-    <div>
-      <h1>{author}</h1>
-      <p>{content}</p>
-      <article className={styles.post}>
-        <header>
-          <div className={styles.author}>
-            <img
-              className={styles.avatar}
-              src="https://github.com/rafinhaa.png"
-              alt="author"
-            />
-            <div className={styles.authorInfo}>
-              <strong>Rafinhaa</strong>
-              <span>Engineer Development</span>
-            </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/rafinhaa.png"
+            alt="author"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Rafinhaa</strong>
+            <span>Engineer Development</span>
           </div>
-          <time
-            title="11 de Maio de 2022 ás 08:13"
-            dateTime="2022-05-11 08:13:00"
-          >
-            Publicado há 1h
-          </time>
-        </header>
-        <div className={styles.content}>
-          <p>
-            Fala galeraa 👋 Acabei de subir mais um projeto no meu portifa. É um
-          </p>
-          <p>projeto que fiz no NLW Return, evento da Rocketseat. O nome do</p>
-          <p>
-            projeto é DoctorCare 🚀 👉 <a href="#">jane.design/doctorcare</a>{" "}
-            <a href="#">#novoprojeto #nlw #rocketseat</a>
-          </p>
         </div>
-      </article>
-    </div>
+        <time
+          title="11 de Maio de 2022 ás 08:13"
+          dateTime="2022-05-11 08:13:00"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>
+          Fala galeraa 👋 Acabei de subir mais um projeto no meu portifa. É um
+        </p>
+        <p>projeto que fiz no NLW Return, evento da Rocketseat. O nome do</p>
+        <p>
+          projeto é DoctorCare 🚀 👉 <a href="#">jane.design/doctorcare</a>{" "}
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
+          <a href="#">#rocketseat</a>{" "}
+        </p>
+      </div>
+    </article>
   );
 };
